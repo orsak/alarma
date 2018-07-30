c2ae739273fa7783edc1b6d1e40ec4354842795b
Carlos Castro Moreno <32720800+orsak@users.noreply.github.com>
2018-07-30 11:58:08 +0200

    subida del archivo alarma en python
    en lugar de print('alarma encendida / apagada')
    debera ir el codigo de python para encender y apagar leds y zumbador

---
+++ alarma.py
@@ -0,0 +1,28 @@
+#################################################
+
+def alarma(n):
+	pass
+	if password != "1234":
+		print('alarma encendida')
+	else:
+		print('alarma apagada')
+
+#################################################
+
+print("Introduzca la contraseña porfavor: ")
+password=input()
+x=1
+if password != "1234" :
+	pass
+	while password != "1234":
+		pass
+		print("Introduzca la contraseña: ")
+		password=input()
+		print(x)
+		x+=1
+		if x == 3 and password != "1234" :
+			pass
+			alarma(password)
+	alarma(password)
+else:
+	alarma(password)
